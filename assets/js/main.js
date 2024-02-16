f5a5656397087406fa63b8b4e59cf77424c71b3e
mattemutti <mattemutti@gmail.com>
2024-02-16 15:01:01 +0100

    add ex 1-2-2B

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+// Ex1 Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console
+
+const number = 100;
+console.log(number);
+
+
+// Ex2 Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+
+const PI = 3.14;
+console.log(PI);
+
+
+// Ex2/B Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+
+// La console darà errore perchè una volta dichiarata non posso cambiare il valore di una const. In quel caso dovrei dichiarare una variabile let
+
+
+
+/* Ex2/C
+
+Dichiara una variabile chiamata radius e assegnale il valore 8.
+Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+Stampa il valore della variabile circle in console.
+*/
+
+
+const radius = 8;
